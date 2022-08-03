80b6067c0034e3b78c3cceb5f47e279562485888
William James Pinto Junior <william.junior@cxmail.com.br>
2022-08-03 09:28:12 -0300

    Initial Commit

---
+++ NetflixSplashScreenSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixSplashScreenSwift
+//
+//  Created by Pinto Junior, William James on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
